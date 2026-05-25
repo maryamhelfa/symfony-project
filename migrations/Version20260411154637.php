c096f0aeabeadd7eca9d2c6a230f75e582717ecb
maryamhelfa <helfamaryam211@gmail.com>
2026-05-25 18:15:51 +0200

    Update Version20260411154637.php

--- migrations/Version20260411154637.php
+++ migrations/Version20260411154637.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20260411154637 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,14 +17,12 @@ final class Version20260411154637 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON user (email)');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE user');
     }
 }
